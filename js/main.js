da1c1e334ca00d81d576245404f46a9ceb7ce8c6
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-14 16:27:45 +0100

    Concluso bonus

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        hello: "hello World!"
+        hello: "Hello World!",
+        img: "https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png",
+        flex: ["flex", "font"]
       }
     }
   }).mount('#app')
